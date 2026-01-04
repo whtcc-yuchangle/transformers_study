1479b529c7e5c42a71d2cc036474f7f57af221a4
yuchangle-whtcc <1043306031@qq.com>
2026-01-04 15:24:11 +0800

    add 26-districuted_dp 04_ddp_trainer_demo.py file

---
+++ 26-districuted_dp/04_ddp_trainer_demo.py
@@ -0,0 +1,60 @@
+import os
+		
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+
+import evaluate
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, BertTokenizer, BertForSequenceClassification, DataCollatorWithPadding
+from datasets import load_dataset
+
+dataset = load_dataset("csv", data_files="./ChnSentiCorp_htl_all.csv", split="train")
+dataset = dataset.filter(lambda x: x["review"] is not None)
+datasets = dataset.train_test_split(test_size=0.1, seed=42)
+
+print(datasets)
+
+tokenizer = BertTokenizer.from_pretrained("/tmp/pretrainmodel/chinese-roberta-wwm-ext")
+
+def process_function(examples):
+    tokenized_examples = tokenizer(examples["review"], max_length=128, truncation=True)
+    tokenized_examples["labels"] = examples["label"]
+    return tokenized_examples
+
+tokenized_datasets = datasets.map(process_function, batched=True, remove_columns=datasets["train"].column_names)
+
+print(tokenized_datasets)
+
+model = BertForSequenceClassification.from_pretrained("/tmp/pretrainmodel/chinese-roberta-wwm-ext")
+
+
+acc_metric = evaluate.load("accuracy")
+f1_metirc = evaluate.load("f1")
+def eval_metric(eval_predict):
+    predictions, labels = eval_predict
+    predictions = predictions.argmax(axis=-1)
+    acc = acc_metric.compute(predictions=predictions, references=labels)
+    f1 = f1_metirc.compute(predictions=predictions, references=labels)
+    acc.update(f1)
+    return acc
+
+train_args = TrainingArguments(output_dir="./checkpoints",      # 输出文件夹
+                               per_device_train_batch_size=128,  # 训练时的batch_size
+                               per_device_eval_batch_size=256,  # 验证时的batch_size
+                               logging_steps=20,                # log 打印的频率
+                               eval_strategy="steps",           # 评估策略
+                               save_strategy="epoch",           # 保存策略
+                               save_total_limit=3,              # 最大保存数
+                               learning_rate=2e-5,              # 学习率
+                               weight_decay=0.01,               # weight_decay
+                               metric_for_best_model="f1",      # 设定评估指标
+                               num_train_epochs=5,
+                               load_best_model_at_end=True)     # 训练完成后加载最优模型
+
+trainer = Trainer(model=model, 
+                  args=train_args, 
+                  train_dataset=tokenized_datasets["train"], 
+                  eval_dataset=tokenized_datasets["test"], 
+                  data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
+                  compute_metrics=eval_metric)
+
+trainer.train()
